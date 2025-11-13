50d9020e6b37915e462f9087c813b41426e1f636
Happyface9698 <nathan.magana@gctcok.edu>
2025-11-13 12:47:44 -0600

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Variables_Magana_Nathan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Variables_Magana_Nathan
+
