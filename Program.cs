8d6bbcba9fcdabb8733a3128c4eca8816372728d
Happyface9698 <nathan.magana@gctcok.edu>
2025-11-13 15:17:04 -0600

    done

--- Program.cs
+++ Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int favoriteNumber;               // Integer (whole number) 
+            bool isJumping = false, isRunning = false; // Boolean (true/false)
+            float myFloat;             // Float (decimal number)
+            favoriteNumber = 420;              // Assigning value to favoriteNumber
+            myFloat = 4.20f;               // Assigning value to myFloat to 42.0
+            const double finalGrade = 42.0; // Constant (unchangeable value) final grade
+
+            // Output the values to the console
+            Console.WriteLine("favorite #: " + favoriteNumber);
+            Console.WriteLine("Jumping: " + isJumping);
+            Console.WriteLine("Running: " + isRunning);
+            Console.WriteLine("Rando Floater Bee: " + myFloat);
+            Console.WriteLine("LaBomb-ba Grande: " + finalGrade);
+            
+
         }
     }
 }
